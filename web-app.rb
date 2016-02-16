788dca6f62137d5c3629f1d5b1f8456523e97f43
Chris Shimmin-Vincent <C.S.Shimmin-Vincent@bath.ac.uk>
2016-02-16 14:37:33 +0000

    added foundation and error page

--- web-app.rb
+++ web-app.rb
@@ -8,7 +8,20 @@ set :static, true
 set :public_folder, File.dirname(__FILE__) + '/static'
 set :views, 'views'
 
-get '/active' do
+get '/active/' do
   initialise
   erb :activecases
+end
+
+get '/case/:caseid' do
+  if params['caseid'] =~ /[0-9]/
+    initialise
+    erb :showcase, :locals => {
+      'caseid' => params['caseid']
+    }
+  else
+    erb :error, :locals => {
+      'error' => 'Invalid case number'
+    }
+  end
 end
